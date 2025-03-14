8778fd84727ff98a9e3d2fcbf00186170d10330a
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-14 16:45:29 -0500

    Update demo.py
    Practice

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 #Follow along with the video. It is the same video for all files.
-#Final out for this file should be "I am done!"
+#Final output for this file should be "I am done!"
 #Link: https://youtu.be/WLkPJNwg528
 
-print("This is not the answer.")
+print("I am done.")
